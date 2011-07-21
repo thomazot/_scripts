9f0a8e7fbed58c733dd658a9b658effff3c165c8
Thomaz Takashi Oda Toyama <contato@thomazot.com.br>
2011-07-21 17:48:42 -0300

    aplicando lightwindow

--- js/jQuery/lightwindow/jquery.lightwindow.js
+++ js/jQuery/lightwindow/jquery.lightwindow.js
@@ -4,34 +4,34 @@
  * @author thomaz takashi oda toyama <contato@thomazot.com.br>
  */
 (function($,window,undefined) {
+	
 	$.fn.lightwindow = function(options) {
-		var config = $.extend({ width: '925', height: '553' , 'winBody': 'body', 'speedyOpen': 'fast', 'speedyClose': 'fast' }, options)
+		var config = $.extend({ width: '925', height: '553' , 'winBody': 'body', 'speedyOpen': 'fast', 'speedyClose': 'fast' }, options) 
+		, self = this
 		, element = function (e) { return document.createElement(e); }
 		, windows = { 
 			instance: function() {
 				if($(config.winBody).find('div.lightwindow-window').length <= 0){
 					var _window = $(element('div'))
 					, y = $(window).height(), x = $(window).width();
-					
+
 					$(config.winBody).css({'min-width': config.width + 'px', 'min-height': config.height + 'px'});
-					
+
 					_window.addClass('lightwindow-window'); 
 					_window.css({ top: (y/2), left: (x/2) });
-					
-					_window.click(function(){
-						_window.close();
-					});
-					
+
 					$(config.winBody).append(_window);
-					
+
 					return _window;
 				} else {
 					return $(config.winBody).find('div.lightwindow-window')
 				}
 			}
-			, open: function() {
+			, open: function(calback) {
 				var _window = windows.instance();
-				_window.animate({ width: config.width, height: config.height, top:'-='+(config.height/2), left: '-='+(config.width/2) },config.speedyOpen);
+				if(calback != undefined) calback();
+				_window.animate({ width: config.width, height: config.height, top:'-='+(config.height/2), left: '-='+(config.width/2) }
+								,config.speedyOpen, function() { });
 			}
 			, close: function() {
 				var _window = windows.instance();
@@ -47,27 +47,25 @@
 					, y = $(window).height(), x = $(window).width();
 					overBody.addClass('lightwindow-overbody'); 
 					overBody.css({'width': x, 'height' : y, display:'none'});
-					
-					overBody.click(function(){
-						windows.close();
-					});
-					
+
+					overBody.click(function(){ windows.close(); });
+
 					$(window).keydown(function(e) {
-						if(e.keyCode = 27 && $(config.winBody).find('div.lightwindow-window').length > 0){
+						if(e.keyCode == 27 && $(config.winBody).find('div.lightwindow-window').length > 0){
 							windows.close();
-						}
+						};
 					});
-					
+
 					$(config.winBody).append(overBody);
-					
+
 					return overBody;
 				}else{
 					return $(config.winBody).find('div.lightwindow-overbody');
-				}
+				};
 			}
-			, open: function() {
+			, open: function(callback) {
 				var _overlay = overlay.instance();
-				_overlay.fadeIn('fast',function() { windows.open(); });
+				_overlay.fadeIn('fast',function() { windows.open(callback); });
 				return false;
 			}
 			, close: function() {
@@ -75,19 +73,39 @@
 				_overlay.fadeOut('fast',function() { _overlay.remove(); });
 			}
 		}
-		lightwindow = {
-			open: function() {
-				overlay.open();
+		, lightwindow = {
+			open: function(callback) {
+				overlay.open(callback);
+				return false;
 			},
 			close: function() {
 				windows.close();
 			}
+		}
+		, loading = {
+			instance: function() {
+				var _window = windows.instance();
+				if($(_window).find('div.lightwindow-loading').length <= 0){
+					var loadwin = element('div');
+
+					loadwin.addClass('lightwindow-loading');
+					loadwin.css({ 'display':'none' });
+					_window.append(loadding);
+
+					return loadwin;
+				}else return $(_window).find('div.lightwindow-loading');
+			}
+			, start: function() {
+				var loadwin = loading.instance();
+
+				loadwin.fadeIn('fast');
+			}
+			,stop: function() {
+				var loadwin = loading.instance();
+
+				loadwin.fadeOut('fast');
+			}
 		};
-		
-		$(this).click(function(){
-			lightwindow.open();
-			return false;
-		});
 		
 		$(window).resize(function(){ 
 			var width 	= $(this).width();
@@ -101,5 +119,38 @@
 			
 		});
 		
+		/**
+		 * Start light window
+		 */
+		$.fn.lightwindow.start = (function( obj, config) {
+			var rel = $(obj).attr('rel');
+			var galery = element('ul');
+			if(rel != '' && rel != undefined) {
+				var _window = windows.instance();
+				$('a[rel="'+rel+'"]',config.winBody).click(function(){ 
+					
+					var href = $(this).attr('href');
+					
+					return lightwindow.open(function(){
+						var _window = windows.instance();
+						var iframe = element('iframe');
+					
+						$(iframe).attr({
+							width: config.width
+							, height: config.height
+							, frameborder: 0
+							, scrolling: 'no'
+							, src: href
+						});
+						
+						$(_window).append(iframe);
+					}); 
+				});
+			};
+			return this;
+		})(this, config);
+		
 	};
+	
+	
 })(jQuery,window);
